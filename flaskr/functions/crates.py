359697937fab624483e9d7fb3d8cd36d32003000
David J White <davwhite@davwhite.com>
2021-05-26 08:27:44 -0400

    Added crates function to demo scripts

--- flaskr/functions/crates.py
+++ flaskr/functions/crates.py
@@ -1,13 +1,15 @@
-import requests
+import requests 
 from bs4 import BeautifulSoup 
-  
-URL = "https://www.x-rates.com/table/?from=USD&amount=1"
-r = requests.get(URL) 
 
-soup = BeautifulSoup(r.content, 'html.parser') 
-ratelist = soup.findAll("table", {"class": "ratesTable"})[0].findAll("tbody")
+def get_rates():
+    URL = "https://www.x-rates.com/table/?from=USD&amount=1"
+    r = requests.get(URL) 
 
-for tableVal in ratelist:
-	trList = tableVal.findAll('tr')
-	for trVal in trList[:6]:
-		print(trVal.text)
+    soup = BeautifulSoup(r.content, 'html.parser') 
+    ratelist = soup.findAll("table", {"class": "ratesTable"})[0].findAll("tbody")
+    rates = []
+    for tableVal in ratelist:
+        trList = tableVal.findAll('tr')
+        for trVal in trList[:6]:
+            rates.append(trVal.text)
+    return str(rates)
